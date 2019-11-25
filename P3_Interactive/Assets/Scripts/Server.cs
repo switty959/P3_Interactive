13aef8b8f1ff729ae3b5ddf879f59b8662793456
Marc Bisgaard Petersen <swittyxxz23@gmail.com>
2019-11-25 15:35:28 +0100

    first testing of opencv connecting to unity

---
+++ P3_Interactive/Assets/Scripts/Server.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+public class Server : MonoBehaviour
+{
+    Thread mThread;
+    public string connectionIP = "127.0.0.1";
+    public int connectionPort = 25001;
+    IPAddress localAdd;
+    TcpListener listener;
+    TcpClient client;
+    Vector3 pos = Vector3.zero;
+    public bool moveObject = false;
+    GameObject thisGameobject;
+
+    bool running;
+
+    private void Update()
+    {
+        if (!moveObject)
+        {
+            transform.position = pos;
+        }
+        else
+        {
+            transform.position = new Vector3(4, 5, 8);
+        }
+    }
+
+    private void Start()
+    {
+        ThreadStart ts = new ThreadStart(GetInfo);
+        mThread = new Thread(ts);
+        mThread.Start();
+
+    }
+
+    public static string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
+    }
+    void GetInfo()
+    {
+        localAdd = IPAddress.Parse(connectionIP);
+        listener = new TcpListener(IPAddress.Any, connectionPort);
+        listener.Start();
+
+        client = listener.AcceptTcpClient();
+
+
+        running = true;
+        while (running)
+        {
+            Connection();
+        }
+        listener.Stop();
+    }
+
+    void Connection()
+    {
+        NetworkStream nwStream = client.GetStream();
+        byte[] buffer = new byte[client.ReceiveBufferSize];
+
+        int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+        string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+        if (dataReceived != null)
+        {
+            if (dataReceived == "stop")
+            {
+                running = false;
+            }
+            else
+            {
+                //pos = StringToVector3(dataReceived);
+                moveObject = bool.Parse(dataReceived);
+                print("moved");
+                print("data from server : " + moveObject);
+                nwStream.Write(buffer, 0, bytesRead);
+            }
+        }
+    }
+
+
+
+    public static Vector3 StringToVector3(string sVector)
+    {
+        // Remove the parentheses
+        if (sVector.StartsWith("(") && sVector.EndsWith(")"))
+        {
+            sVector = sVector.Substring(1, sVector.Length - 2);
+        }
+
+        // split the items
+        string[] sArray = sVector.Split(',');
+
+        // store as a Vector3
+        Vector3 result = new Vector3(
+            float.Parse(sArray[0]),
+            float.Parse(sArray[1]),
+            float.Parse(sArray[2]));
+
+        return result;
+    }
+}
