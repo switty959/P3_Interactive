a96c23eb3fea5fdaf0e0a521a3e6117f09486b81
Scott Naylor <scottjamesnaylor@gmail.com>
2019-11-27 11:03:16 +0100

    Client modified for unreal

--- OpenCVClient/OpenCVClient/.ipynb_checkpoints/OpenCVClient-checkpoint.py
+++ OpenCVClient/OpenCVClient/.ipynb_checkpoints/OpenCVClient-checkpoint.py
@@ -4,7 +4,7 @@ import socket
 host, port = "127.0.0.1", 25001
 data = "true"
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #the database of faces
 faceDetect = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_default.xml')
@@ -18,7 +18,6 @@ s.connect((host, port))
 
 while True:
     count = 0
-    countFace = 0
     #two variables that is reading the camera
     ret,img = cam.read() 
 
@@ -33,35 +32,28 @@ while True:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
         roi_grey = gray[y:y+h,x:x+w]
         roi_color = img[y:y+h,x:x+w]
-        countFace +=1
         smiles = smileDetect.detectMultiScale(roi_grey,1.8,15)
-        #eyes = eyeDetect.detectMultiScale(roi_grey,1.5,9)
+        eyes = eyeDetect.detectMultiScale(roi_grey,1.5,9)
         
         for(sx,sy,sw,sh) in smiles:
             count +=1
             cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 255, 0), 2)
-       # for(ex,ey,ew,eh) in eyes:
-            #cv2.rectangle(roi_color,(ex,ey), ((ex + ew), (ey + eh)), (255, 0, 0), 2)
+        for(ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey), ((ex + ew), (ey + eh)), (255, 0, 0), 2)
         
       
 
     #creating a window with a name and what should be displayed
     cv2.imshow("Face",img)
-    #print(count)
-    if  countFace > 0:
-        data = "Face without smile"
-
-        if count > 0:
-
-            data = "Face with smile"
-
+    print(count)
+    if count > 0:
+        data = "true"
     else :
-
-        data = "No face detected"
+        data = "false"
 
     s.sendall(data.encode("utf-8"))
-    #Data = s.recv(1024).decode("utf-8")
-    print(countFace)
+    Data = s.recv(1024).decode("utf-8")
+    #print(data)
         
 
     #killing the windows with webcam feedback
